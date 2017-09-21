ce2beda5e3ee67f4672e0bd602646cde8c0b6704
OlegPalchyk <olegpalchyk@gmail.com>
2017-09-22 02:48:22 +0300

    create formValidation on add

--- src/containers/createItem/createItem.js
+++ src/containers/createItem/createItem.js
@@ -3,10 +3,9 @@ import {Button, FormControl, HelpBlock, ControlLabel, FormGroup} from 'react-boo
 import {connect} from "react-redux";
 import {addItem, ADD_PRODUCT_SUCCESS} from "../../actions/products";
 import './createItem.css';
-
-const FieldGroup = ({id, label, help, ...props})=> {
+const FieldGroup = ({id, label, help, validationState, ...props})=> {
     return (
-        <FormGroup controlId={id}>
+        <FormGroup controlId={id} validationState={validationState}>
             <ControlLabel>{label}</ControlLabel>
             <FormControl {...props} />
             {help && <HelpBlock>{help}</HelpBlock>}
@@ -20,13 +19,46 @@ class CreateItem extends Component {
             title: "",
             price: "",
             description: "",
-            file: ""
+            file: "",
+
+            showValid: false,
+            errorFields : { }
         }
     }
+    invalidForm(){
+        let invalid = false;
+        let errorFields = {};
+        if(this.state.title.match((/^\s*$/)) || this.state.title.length === 0){
+            errorFields.title = true
+            invalid = true;
+        }
+        if(this.state.description.match((/^\s*$/)) || this.state.description.length === 0){
+            errorFields['description'] = true;
+            invalid = true;
+        }
+        if(this.state.price.match((/^\s*$/)) || this.state.price.length === 0 || !this.state.price.match(/^(\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/)){
+            errorFields['price'] = true;
+            invalid = true;
 
+        }
+        if(this.state.file.length === 0 ){
+            errorFields['file'] = true;
+            invalid = true;
+
+        }
+
+        return !invalid?false : errorFields;
+    }
     addItem(e) {
-
         e.preventDefault();
+        let a = this.invalidForm();
+        if(a){
+            this.setState({
+                errorFields : a
+            });
+            return;
+        }
+
         let newItem = {
             "title": this.state.title,
             "price": +this.state.price,
@@ -38,32 +70,63 @@ class CreateItem extends Component {
 
     handleChange(event) {
 
+
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
+        let errorFields = this.state.errorFields;
+        if(name!=='price'){
+
+            if(value.match((/^\s*$/)) || value.length === 0){
+                errorFields[name] = true
+            }else{
+                errorFields[name] = false
+            }
+        }else{
+            if( value.match((/^\s*$/)) || value.length === 0 || !value.match(/^(\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/)){
+                errorFields[name] = true
+            }else{
+                errorFields[name] = false
+            }
+        }
 
         this.setState({
-            [name]: value
+            [name]: value,
+            errorFields
         });
     }
 
     readFile(event) {
+
         let reader = new FileReader();
         let file = event.target.files[0];
+        let fileType = file.type;
+        let size = file.size;
+        let imageTYpes = ["image/gif", "image/jpeg", "image/png"];
+        if(imageTYpes.indexOf(fileType) === -1 || size>=1024*1024*5 ){
+            let errorFields =this.state.errorFields;
+            errorFields.file = true;
+            return ;
+        }
+
         reader.onload = (file)=> {
             this.setState({
-                file: file.target.result
+                file: file.target.result,
+
             })
         };
 
         reader.readAsDataURL(file);
     }
 
-    componentWillReceiveProps(nextProps){
-        if(nextProps.products.type === ADD_PRODUCT_SUCCESS){
+
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.products.type === ADD_PRODUCT_SUCCESS) {
             this.props.history.push(`/products/${nextProps.products.lastAddedItemId}`);
         }
     }
+
     render() {
         return (
             <div className="create-item">
@@ -71,49 +134,58 @@ class CreateItem extends Component {
                     <FieldGroup
                         id="formControlsTitle"
                         type="text"
-                        label="Text"
-                        placeholder="Enter text"
+                        label="Title"
+                        placeholder="Enter Title"
                         name="title"
                         onChange={(e)=> {
                             this.handleChange(e)
                         }}
-
+                        validationState={'title' in this.state.errorFields? this.state.errorFields.title?"warning":"success" : null}
+                        help={this.state.errorFields.title?'Required': false}
                     />
                     <FieldGroup
                         id="formControlsPrice"
-                        label="text"
+                        label="Price"
                         type="text"
                         placeholder="Enter price"
                         name="price"
                         onChange={(e)=> {
                             this.handleChange(e)
                         }}
+                        validationState={'price' in this.state.errorFields? this.state.errorFields.price?"warning":"success" : null}
+                        help={this.state.errorFields.price?'Required and can contain only numbers': false}
+
 
                     />
                     <FieldGroup
                         id="formControlsFile"
                         type="file"
                         label="File"
-                        help="Example block-level help text here."
                         name="file"
+                        accept="image/*"
                         onChange={(e)=> {
                             this.readFile(e)
                         }}
-                    />
+                        help={this.state.errorFields.file?'Image is required and max size is 3mb': false}
+                        validationState={'price' in this.state.errorFields? this.state.errorFields.file?"warning":"success" : null}
 
-                    <FormGroup controlId="formControlsTextarea">
-                        <ControlLabel>Textarea</ControlLabel>
-                        <FormControl componentClass="textarea" placeholder="textarea" name="description"
+                    />
+                    <FormGroup controlId="formControlsTextarea" validationState={'description' in this.state.errorFields? this.state.errorFields.description?"warning":"success" : null}>
+                        <ControlLabel>Description</ControlLabel>
+                        <FormControl componentClass="textarea" placeholder="Enter description" name="description"
                                      onChange={(e)=> {
                                          this.handleChange(e)
-                                     }}/>
+                                     }}
+                        />
+                        {this.state.errorFields.description?<HelpBlock>{'Required'}</HelpBlock>: null}
+
+
                     </FormGroup>
-
-
                     <Button type="submit">
                         Submit
                     </Button>
                 </form>
+
             </div>
         );
     }
